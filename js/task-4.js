67005439496770ec371a085c78cad3fd685fa87b
d-levchenko <dl.qubb@gmail.com>
2026-03-06 16:42:20 +0200

    tasks completed

---
+++ js/task-4.js
@@ -0,0 +1,30 @@
+function getShippingCost(country) {
+  'use strict';
+  let costToCountry;
+
+  switch (country) {
+    case 'Australia':
+      costToCountry = 170;
+      break;
+    case 'Chile':
+      costToCountry = 250;
+      break;
+    case 'China':
+      costToCountry = 100;
+      break;
+    case 'Jamaica':
+      costToCountry = 120;
+      break;
+    default:
+      return 'Sorry, there is no delivery to your country';
+  }
+
+  return `Shipping to ${country} will cost ${costToCountry} credits`;
+}
+
+console.log(getShippingCost('Australia')); // "Shipping to Australia will cost 170 credits"
+console.log(getShippingCost('Germany')); // "Sorry, there is no delivery to your country"
+console.log(getShippingCost('China')); // "Shipping to China will cost 100 credits"
+console.log(getShippingCost('Chile')); // "Shipping to Chile will cost 250 credits"
+console.log(getShippingCost('Jamaica')); // "Shipping to Jamaica will cost 120 credits"
+console.log(getShippingCost('Sweden')); // "Sorry, there is no delivery to your country"
