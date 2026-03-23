0dda75991fc957e8efd52d5524ccbd9990b1e2db
d-levchenko <dl.qubb@gmail.com>
2026-03-23 15:17:21 +0200

    fixed return string

--- js/task-1.js
+++ js/task-1.js
@@ -4,7 +4,7 @@ function makeTransaction(quantity, pricePerDroid, customerCredits) {
   let totalPrice = quantity * pricePerDroid;
 
   if (customerCredits >= totalPrice) {
-    return `You ordered ${quantity} droids worth ${totalPrice} credits`;
+    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
   } else {
     return 'Insufficient funds!';
   }
